ba5ae689cd5772c4a3f71d26664766b5ec03a465
Bohdan Davydenko <bodia2016151@gmaul.com>
2025-04-28 22:17:31 +0300

    include iostream and cout git

--- main.cpp
+++ main.cpp
@@ -5,23 +5,11 @@ using namespace std;
 int main(){
     
 
-    cout<<"Begin"<<endl;
+  cout << "Git hello \n";
  
     
-    Array arr;  
-
-  //  arr.setValue(10);  
-   arr.setValue(20);
-  arr.setValue(30);
-  
-    //arr.showElements();  
-//arr.expandArray(1);
-    //int newArr[] = {20, 44};
-   // arr.setArray(newArr, 2);  
-    //arr.showElements();  
-
-
-
+    
+ 
 
 
 
